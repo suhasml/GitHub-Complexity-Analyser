27885b3f37d6e5912efe28dc369582aa703da38a
Suhas M L <77441851+suhasml@users.noreply.github.com>
2023-10-07 13:09:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -195,37 +195,37 @@ def preprocess_code_cell(cell):
 
     return cell["source"]
 
-# def generate_prompt(repository, code):
-#     #the models max token count is 2048, so we need to limit the token count to 2000
-#     if len(code.split()) > 100:
-#         code = " ".join(code.split()[:100])
-    
-#     prompt = f"""
-#     Generate a code complexity score for the following code snippet:
-#     --------------------------------------------------
-#     Code:
-#     {code}
-#     --------------------------------------------------
-
-#     """
-
-#     return prompt
-
 def generate_prompt(repository, code):
     #the models max token count is 2048, so we need to limit the token count to 2000
     if len(code.split()) > 100:
         code = " ".join(code.split()[:100])
     
-    Prompt = PromptTemplate.from_template(
-        """Generate a code complexity score for the following code snippet:
-        --------------------------------------------------
-        Code:
-        {code}
-        --------------------------------------------------
-        """
-    )
-    prompt = Prompt.render(code=code)
+    prompt = f"""
+    Generate a code complexity score for the following code snippet:
+    --------------------------------------------------
+    Code:
+    {code}
+    --------------------------------------------------
+
+    """
+
     return prompt
+
+# def generate_prompt(repository, code):
+#     #the models max token count is 2048, so we need to limit the token count to 2000
+#     if len(code.split()) > 100:
+#         code = " ".join(code.split()[:100])
+    
+#     Prompt = PromptTemplate.from_template(
+#         """Generate a code complexity score for the following code snippet:
+#         --------------------------------------------------
+#         Code:
+#         {code}
+#         --------------------------------------------------
+#         """
+#     )
+#     prompt = Prompt.render(code=code)
+#     return prompt
 
 # Use GPT-3 to analyze the code
 def analyze_code(prompts):
@@ -283,36 +283,11 @@ def identify_most_complex_repository(repositories):
         return None, 0, "", None
 
 
-# def generate_justification(repository):
-#     #the models max token count is 2048, so we need to limit the token count to 2000
-#     if len(repository.split()) > 2000:
-#         repository = " ".join(repository.split()[:2000])
-#     prompt = f"Justify why the repository '{repository}' is considered the most complex:"
-
-#     response = openai.Completion.create(
-#         engine="babbage",
-#         prompt=prompt,
-#         max_tokens=200,
-#         temperature=0.7,
-#         n=1,
-#         stop=None
-#     )
-
-#     if response.choices:
-#         justification = response.choices[0].text.strip()
-#         return justification
-#     else:
-#         return ""
-
 def generate_justification(repository):
     #the models max token count is 2048, so we need to limit the token count to 2000
     if len(repository.split()) > 2000:
         repository = " ".join(repository.split()[:2000])
-    Prompt = PromptTemplate.from_template(
-        """Justify why the repository '{repository}' is considered the most complex:
-        """
-    )
-    prompt = Prompt.render(repository=repository)
+    prompt = f"Justify why the repository '{repository}' is considered the most complex:"
 
     response = openai.Completion.create(
         engine="babbage",
@@ -328,6 +303,31 @@ def generate_justification(repository):
         return justification
     else:
         return ""
+
+# def generate_justification(repository):
+#     #the models max token count is 2048, so we need to limit the token count to 2000
+#     if len(repository.split()) > 2000:
+#         repository = " ".join(repository.split()[:2000])
+#     Prompt = PromptTemplate.from_template(
+#         """Justify why the repository '{repository}' is considered the most complex:
+#         """
+#     )
+#     prompt = Prompt.render(repository=repository)
+
+#     response = openai.Completion.create(
+#         engine="babbage",
+#         prompt=prompt,
+#         max_tokens=200,
+#         temperature=0.7,
+#         n=1,
+#         stop=None
+#     )
+
+#     if response.choices:
+#         justification = response.choices[0].text.strip()
+#         return justification
+#     else:
+#         return ""
 
 # Streamlit app
 def main():
